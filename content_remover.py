80d0178e6f2889c11e18b7782375ea5448925803
nynra <nynradev@pm.mecd>
2025-09-28 21:37:15 +0200

    refactored clean_stale_content

--- content_remover.py
+++ content_remover.py
@@ -39,7 +39,7 @@ class ContentRemover(object):
         self._account: MyPlexAccount = self._server.myPlexAccount()
 
         # Check if the server is reachable
-        if not self.server_reachable:
+        if not self._account.ping():
             logging.error("Plex server is not reachable")
             raise ConnectionError("Plex server is not reachable")
 
@@ -56,36 +56,33 @@ class ContentRemover(object):
                 raise ValueError(f"Library section {section} not found")
         return
 
-    # Properties
-    @property
-    def server_reachable(self) -> bool:
-        return self._account.ping()
-
-    @property
-    def dry_run(self) -> bool:
-        return self._dry_run
-
-    @property
-    def verbose(self) -> bool:
-        return self._verbose
-
     # Methods
-    def clean_stale_content(self) -> None:
+    def get_stale_content(self, filtered: bool = True) -> dict[str, list[str]]:
         stale_content = {}
         for section in self._library_sections_to_search:
-            stale_content[section] = self._get_stale_content(section)
+            stale_content[section] = self._get_stale_content(section, filtered=filtered)
+
+        if logger.isEnabledFor(logging.DEBUG):
+            for section, stale_items in stale_content.items():
+                logging.debug(
+                    f"Found {len(stale_items)} stale items in {section}: {[item.title for item in stale_items]}"
+                )
+        return stale_content
+
+    def clean_stale_content(self) -> None:
+        stale_content = self.get_stale_content()
 
         if self._dry_run:
             logging.warning("Dry run enabled, no content will be removed")
-            for section, stale_items in stale_content.items():
-                logging.info(
-                    f"Found {len(stale_items)} stale items in {section}: {[item.title for item in stale_items]}"
-                )
-
+            if logger.isEnabledFor(logging.INFO):
+                for section, stale_items in stale_content.items():
+                    logging.info(
+                        f"Found {len(stale_items)} stale items in {section}: {[item.title for item in stale_items]}"
+                    )
             return
 
     # Support functions
-    def _get_stale_content(self, library_name: str) -> list[str]:
+    def _get_stale_content(self, library_name: str, filtered: bool = True) -> list[str]:
         """Return a list of stale content ids.
 
         Content is considered stale is it exceeds the number of days since adding
@@ -100,17 +97,18 @@ class ContentRemover(object):
             filters={"addedAt<<": thresh_date.strftime("%Y-%m-%d")}
         )
         # Filter out watchlisted or favorited content if configured to do so
-        if self._config.getboolean(library_name, "keep_watchlisted"):
+        if self._config.getboolean(library_name, "keep_watchlisted") and filtered:
             stale_content = self._remove_watchlisted(
                 user=self._account,
                 stale_content=stale_content,
+                lib_section=lib_section,
             )
 
         # Filter out content in collections if configured to do so
         keep_collections = literal_eval(
             self._config.get(library_name, "keep_collections")
         )
-        if len(keep_collections) > 0:
+        if len(keep_collections) > 0 and filtered:
             stale_content = self._remove_collections(
                 stale_content=stale_content,
                 collections=keep_collections,
@@ -119,28 +117,28 @@ class ContentRemover(object):
 
         return stale_content
 
-    def _remove_watchlisted(self, user: MyPlexAccount, stale_content: list) -> list:
+    def _remove_watchlisted(
+        self, user: MyPlexAccount, stale_content: list, lib_section: LibrarySection
+    ) -> list:
         # Get the watchlisted content
         watchlist = user.watchlist()
 
         logging.debug(f"Found {len(watchlist)} items in the watchlist")
 
         for item in watchlist:
-            result = self._server.library.search(guid=item.guid, libtype=item.type)
+            result = lib_section.search(guid=item.guid)
             if len(result) == 0:
                 # Movie is watchlisted but not in the library
                 continue
 
             logging.debug(f"Found watchlisted item {item.title} on server")
 
-            removed = []
             for res in result:
                 if res in stale_content:
                     logging.debug(
                         f"Removing watchlisted item {res.title} from stale list"
                     )
                     stale_content.remove(res)
-                    removed.append(res.title)
 
         return stale_content
 
@@ -149,7 +147,6 @@ class ContentRemover(object):
     ) -> list:
         logging.debug(f"Checking {len(collections)} collections to keep")
 
-        # Get the collection items
         in_collections = (
             []
         )  # the collection items that are actually in these collections
@@ -179,11 +176,8 @@ class ContentRemover(object):
 
 
 if __name__ == "__main__":
+    # Run the cleaner with the config file in the same directory
+    # THIS WILL DELETE CONTENT IF DRY RUN IS DISABLED
     path = os.path.join(os.path.dirname(__file__), "config.ini")
     remover = ContentRemover(config_path=path)
     remover.clean_stale_content()
-
-    # section: LibrarySection = remover._server.library.section("movies")
-    # print(section.collections())
-    # print("---- results ----")
-    # print(section.search(guid=678))
